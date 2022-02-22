d29ee8c6053bb77b3bab2d142631e73301bd6954
Michael Chung <mikechung89@gmail.com>
2022-02-22 13:09:12 -0500

    commit tree 6132df1a7db16383cc5fe1f3f476e6eacb679b78

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ft){
   // Write your code here!
-}
+if (ft <= 400) {
+  return "This one is on me!"
+} else if (ft > 2000 && ft <= 2500) {
+  return "I will gladly take your thirty bucks."
+} else if (ft > 2500)
+ {
+   return "No can do."
+ }}
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let reply
+city === "NYC" ? (reply = "Ok, sounds good.") : (reply = "No go.")
+return reply
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case `generous`:
+      return "Thank you so much.";
+    case `not as generous`:
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
